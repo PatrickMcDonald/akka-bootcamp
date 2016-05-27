a3f78f2b66d81c0115526a1db147cd2e907d9d25
Patrick McDonald <paddymcdonald@gmail.com>
2016-05-27 21:39:15 +0100

    Complete Lesson 2.3

---
+++ src/Unit-2/DoThis/Actors/ButtonToggleActor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    public class ButtonToggleActor : UntypedActor
+    {
+        #region Message Types
+        /// <summary>
+        /// Toggles this button on or off and sends an appropriate messages
+        /// to the <see cref="PerformanceCounterCoordinatorActor"/>
+        /// </summary>
+        public class Toggle { }
+        #endregion
+
+        private readonly CounterType _myCounterType;
+        private bool _isToggledOn;
+        private readonly Button _myButton;
+        private readonly IActorRef _coordinatorActor;
+
+        public ButtonToggleActor(IActorRef coordinatorActor, Button myButton, CounterType myCounterType, bool isToggledOn = false)
+        {
+            _coordinatorActor = coordinatorActor;
+            _myButton = myButton;
+            _isToggledOn = isToggledOn;
+            _myCounterType = myCounterType;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Toggle && _isToggledOn)
+            {
+                // Toggle is currently on
+
+                // Stop watching this counter
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Unwatch(_myCounterType));
+
+                FlipToggle();
+            }
+            else if (message is Toggle && !_isToggledOn)
+            {
+                // Toggle is currently on
+
+                // Stop watching this counter
+                _coordinatorActor.Tell(new PerformanceCounterCoordinatorActor.Watch(_myCounterType));
+
+                FlipToggle();
+            }
+            else
+            {
+                Unhandled(message);
+            }
+        }
+
+        private void FlipToggle()
+        {
+            // Flip the toggle
+            _isToggledOn = !_isToggledOn;
+
+            // Change the text of the button
+            _myButton.Text = string.Format("{0} ({1})", _myCounterType.ToString().ToUpperInvariant(), _isToggledOn ? "ON" : "OFF");
+        }
+    }
+}
---
+++ src/Unit-2/DoThis/Actors/PerformanceCounterActor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    /// <summary>
+    /// Actor responsible for monitoring a specific <see cref="PerformanceCounter"/>
+    /// </summary>
+    public class PerformanceCounterActor : UntypedActor
+    {
+        private readonly string _seriesName;
+        private readonly Func<PerformanceCounter> _performanceCounterGenerator;
+        private PerformanceCounter _counter;
+
+        private readonly HashSet<IActorRef> _subscriptions;
+        private readonly ICancelable _cancelPublishing;
+
+        public PerformanceCounterActor(string seriesName, Func<PerformanceCounter> performanceCounterGenerator)
+        {
+            _seriesName = seriesName;
+            _performanceCounterGenerator = performanceCounterGenerator;
+            _subscriptions = new HashSet<IActorRef>();
+            _cancelPublishing = new Cancelable(Context.System.Scheduler);
+        }
+
+        #region Actor lifecycle methods
+        protected override void PreStart()
+        {
+            // Create a new instance of the performance counter
+            _counter = _performanceCounterGenerator();
+            Context.System.Scheduler.ScheduleTellRepeatedly(
+                TimeSpan.FromMilliseconds(250),
+                TimeSpan.FromMilliseconds(250),
+                Self,
+                new GatherMetrics(),
+                Self,
+                _cancelPublishing);
+        }
+
+        // prevent resource leaks by disposing of our current PeformanceCounter
+        protected override void PostStop()
+        {
+            try
+            {
+                // Terminate the scheduled task
+                _cancelPublishing.Cancel(false);
+                _counter.Dispose();
+            }
+            catch
+            {
+                // Don't care about additional "ObjectDisposed" exceptions
+            }
+            finally
+            {
+                base.PostStop();
+            }
+        }
+        #endregion
+
+        protected override void OnReceive(object message)
+        {
+            if (message is GatherMetrics)
+            {
+                // Publish latest counter value to all subscribers
+                var metric = new Metric(_seriesName, _counter.NextValue());
+                foreach (var sub in _subscriptions)
+                {
+                    sub.Tell(metric);
+                }
+            }
+            else if (message is SubscribeCounter)
+            {
+                // Add a subscription for this counter
+                // (it's parent's job to filter by counter types)
+                var sc = message as SubscribeCounter;
+                _subscriptions.Add(sc.Subscriber);
+            }
+            else if (message is UnsubscribeCounter)
+            {
+                // Remove a subscription from this counter
+                var uc = message as UnsubscribeCounter;
+                _subscriptions.Remove(uc.Subscriber);
+            }
+        }
+    }
+}
